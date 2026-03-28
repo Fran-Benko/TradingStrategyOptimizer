b131664e6fc5b519090c62ff4141d647851c301f
Franco Yair Benko <fran.benko@gmail.com>
2026-03-28 23:19:28 +0000

    Update: Backtesting module exports

--- src/trading_system/backtesting/__init__.py
+++ src/trading_system/backtesting/__init__.py
@@ -8,10 +8,24 @@ from trading_system.backtesting.engine import (
     BacktestResult,
     Trade,
 )
+from trading_system.backtesting.metrics import (
+    AdvancedMetrics,
+    calculate_advanced_metrics,
+)
+from trading_system.backtesting.reporting import (
+    ReportGenerator,
+    ReportConfig,
+    ChartConfig,
+)
 
 __all__ = [
     "BacktestEngine",
     "BacktestConfig",
     "BacktestResult",
     "Trade",
+    "AdvancedMetrics",
+    "calculate_advanced_metrics",
+    "ReportGenerator",
+    "ReportConfig",
+    "ChartConfig",
 ]
